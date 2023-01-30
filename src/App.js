367e19b958e8f27e1a7eedeaab9937d64c436116
Kristapinoo <kristapskostukevics@gmail.com>
2023-01-31 00:17:18 +0200

    Ai ai ai
    Da nu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState({});
 
-  let getLink = () => {
-    
-    fetch('https://kakis123-eiddpp.5sc6y6-2.usa-e2.cloudhub.io/getRandomCar')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      
-    })
-  }
+  useEffect(() => {
+    fetch("https://kakis123-eiddpp.5sc6y6-2.usa-e2.cloudhub.io/getRandomCar")
+      .then(response => response.json())
+      .then(responseData => setData(responseData));
+  }, []);
 
-  
-  
-
+  return (
+    <div>
+      {Object.keys(data).map(key => (
+        <div key={key}>
+          {key}: {data[key]}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
